9cff5ed0f8bf62faf83d7ed7ac33ef542babef12
Eduardo Nogueira <eduardodudunog@gmail.com>
2022-02-22 22:32:53 -0300

    Error handling with and without react-query

--- src/components/RQSuperHeroes.tsx
+++ src/components/RQSuperHeroes.tsx
@@ -3,14 +3,18 @@ import { useQuery } from 'react-query'
 import { SuperHeroes } from '../interfaces/super-heroes'
 
 export function RQSuperHeroesPage() {
-  const { isLoading, data } = useQuery<SuperHeroes[]>('super-heroes', async () => {
-    const response = await axios.get('http://localhost:4000/superheroes')
+  const { isLoading, data, isError, error } = useQuery<SuperHeroes[]>('super-heroes', async () => {
+    const response = await axios.get('http://localhost:4000/superheroesa')
 
     return response.data
   })
 
   if (isLoading) {
     return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <h2>{(error as TypeError).message}</h2>
   }
 
   return (
--- src/components/SuperHeroes.tsx
+++ src/components/SuperHeroes.tsx
@@ -4,19 +4,28 @@ import { SuperHeroes } from '../interfaces/super-heroes'
 
 export function SuperHeroesPage() {
   const [data, setData] = useState<SuperHeroes[]>([])
-
   const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    axios.get<SuperHeroes[]>('http://localhost:4000/superheroes')
+    axios.get<SuperHeroes[]>('http://localhost:4000/superheroesa')
       .then(response => {
         setData(response.data)
+      })
+      .catch(error => {
+        setError(error.message)
+      })
+      .finally(() => {
         setIsLoading(false)
       })
   }, [])
 
   if (isLoading) {
     return <h2>Loading...</h2>
+  }
+
+  if (error) {
+    return <h2>{error}</h2>
   }
 
   return (
