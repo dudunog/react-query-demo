7c50307960404cba0df9ab3245f6eb9f5c00d68e
Eduardo Nogueira <eduardodudunog@gmail.com>
2022-02-22 22:09:10 -0300

    With React Query library

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,37 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import { SuperHeroesPage } from './components/SuperHeroes'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes'
 import { HomePage } from './components/Home'
+import { queryClient } from './services/queryClient'
+import { QueryClientProvider } from "react-query"
 
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/super-heroes'>Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/super-heroes' element={<SuperHeroesPage />} />
-          <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/super-heroes' element={<SuperHeroesPage />} />
+            <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- src/components/RQSuperHeroes.tsx
+++ src/components/RQSuperHeroes.tsx
@@ -1,3 +1,25 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import { SuperHeroes } from '../interfaces/super-heroes'
+
 export function RQSuperHeroesPage() {
-  return <div>RQ Super Heroes Page</div>
+  const { isLoading, data } = useQuery<SuperHeroes[]>('super-heroes', async () => {
+    const response = await axios.get('http://localhost:4000/superheroes')
+
+    return response.data
+  })
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+
+      {data?.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>
+      })}
+    </>
+  )
 }
